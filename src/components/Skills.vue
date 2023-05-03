cc72235690f2f8f23cf1756ebb2ef269bcb0db3d
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-05-03 08:29:35 +0700

    fix responsive

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -7,7 +7,7 @@ import SkillCard from '../widgets/SkillCard.vue';
     <!-- <img src="/skills/bottom-star.svg" alt="star element" class=" absolute left-0 top-600 w-44 lg:w-48 2xl:w-64"> -->
     <div class="px-8 pt-8 pb-10 xl:px-20">
       <p class="text-3xl text-white font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl">Skills</p>
-      <p class="text-white text-center py-4">Through years of education and hands-on experience, I've honed a diverse range of skills that enable me to tackle complex challenges with confidence and creativity. Here are some of the skills I've developed:</p>
+      <p class="text-white text-center py-4 md:text-xl 2xl:text-2xl">Through years of education and hands-on experience, I've honed a diverse range of skills that enable me to tackle complex challenges with confidence and creativity. Here are some of the skills I've developed:</p>
       <div class="flex flex-wrap mb-4">
         <div class="w-1/2 md:w-1/3 lg:w-1/4">
           <SkillCard
