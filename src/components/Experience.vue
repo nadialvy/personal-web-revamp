7aa8765251ceddb1912d05153875e1cc5c83719c
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-05-04 10:24:38 +0700

    fix minor ui on experiences

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="bg-purple-500 text-white px-8 py-10 xl:px-64">
+  <div class="bg-purple-500 text-white px-8 py-10 lg:px-20 xl:px-64 2xl:px-[410px]">
     <p
       class="text-3xl text-white font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl pb-8"
     >
@@ -22,11 +22,11 @@
             <ul>• Upgraded SIORDIG's backend application to v2, in order to optimize runtime and improve code cleanliness.</ul>
             <h3 class="mt-4 font-semibold text-yellow-500 mb-2">Related Techstacks</h3>
             <div class="flex gap-x-4 flex-wrap gap-y-2 pb-2">
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Flutter</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Typescript</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Express JS</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Mongo DB</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Github</button>
+              <button class="border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Flutter</button>
+              <button class="border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Typescript</button>
+              <button class="border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Express JS</button>
+              <button class="border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Mongo DB</button>
+              <button class="border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Github</button>
             </div>
           </div>
       </li>
@@ -44,10 +44,10 @@
             <ul>• Fix crash/error on production environments to improve application stability and reduce the possibility of errors occurring on the user side.</ul>
             <h3 class="mt-4 font-semibold text-yellow-500 mb-2">Related Techstacks</h3>
             <div class="flex gap-x-4 flex-wrap gap-y-2 pb-2">
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Flutter</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">GetX</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Jira</button>
-              <button class="rounded-full border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Gitlab</button>
+              <button class=" border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Flutter</button>
+              <button class=" border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">GetX</button>
+              <button class=" border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Jira</button>
+              <button class=" border px-4 text-sm border-white hover:border-pink-700 hover:bg-pink-700">Gitlab</button>
             </div>
           </div>
       </li>
