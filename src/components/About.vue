8e0b838cfb6aabb793f715f4f2b52c969ef6e7ee
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-05-06 11:39:03 +0700

    fix background color

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,19 @@
 <template>
-  <p class="relative bottom-[68px] lg:bottom-[98px] bg-white" id="about"></p>
+  <p class="relative bottom-[68px] lg:bottom-[98px]" id="about"></p>
   <img src="/about/el-planet.svg" alt="PLanet element" class="w-28 absolute right-0 lg:w-32 xl:w-52 2xl:w-64">
-  <p class="text-3xl text-purple-500 pb-8 pt-8 font-bold text-center lg:hidden">About</p>
-  <div class="px-8 py-2 md:flex md:gap-x-4 xl:gap-x-8 lg:px-20 xl:px-24 2xl:px-72 2xl:gap-x-16 lg:mt-6">
+  <p class="text-3xl text-purple-500 pb-8 pt-8 font-bold text-center lg:hidden bg-white">About</p>
+  <div class="relative px-8 py-2 md:flex md:gap-x-4 xl:gap-x-8 lg:px-20 xl:px-24 2xl:px-72 2xl:gap-x-16 lg:mt-6 bg-white">
     <div class="md:w-1/3 items-center md:justify-center">
       <img src="/about/main-pict.png" alt="Self Picture" class="w-full animate-bounce animate-bounce-slow ease-in">
     </div>
-    <div class="pt-8 text-purple-500 md:w-2/3 md:text-lg lg:text-lg xl:text-2xl">
+    <div class="pt-8 text-purple-500 md:w-2/3 md:text-lg lg:text-lg xl:text-2xl bg-white pb-24">
       <p class="text-white hidden lg:block lg:text-purple-500 font-bold lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl hover-rectangle hover:cursor-pointer overflow-x-hidden">About</p>
       <p>Exploring my world of turning challenges into new comfort zones! My name is Nadia Lovely, and I am a Flutter Developer with a passion for pushing my limits. Over the past 2+ years, I've been learning the ins and outs of web programming with Laravel and VueJS.</p>
       <p class="py-4 ">Recently, I have been working as a Fullstack Developer, gaining valuable experience and developing my skills after completing my internship at a start-up where I built mobile applications as a Flutter Developer.</p>
       <p>When I'm not coding, you can find me indulging in my love for k-pop, playing with stray cats, and listening to music.</p>
     </div>
+    <img src="/about/el-star.svg" alt="Star" class="absolute bottom-0 left-0 w-44 lg:w-52 2xl:w-64">
   </div>
-  <img src="/about/el-star.svg" alt="Star" class="w-44 lg:w-52 2xl:w-64">
 </template>
 
 <style>
--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,6 +1,6 @@
 <template>
-  <p class="relative bottom-[68px] lg:bottom-[98px] bg-white" id="contact"></p>
-  <div class="py-6 px-8 lg:px-20 lg:py-8 xl:px-28 xl:py-10 2xl:px-[410px] 2xl:py-12 relative mb-10">
+  <p class="relative bottom-[68px] lg:bottom-[98px]" id="contact"></p>
+  <div class="py-6 px-8 lg:px-20 lg:py-8 xl:px-28 xl:py-10 2xl:px-[410px] 2xl:py-12 relative pb-10 bg-white">
     <p class="text-3xl text-purple-500 font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl xl:pb-12">Let's Work Together</p>
     <div class="lg:flex lg:gap-x-24">
       <div class="lg:w-1/2 xl:text-xl">
@@ -30,25 +30,25 @@
           <label class="label">
             <span class="label-text lg:text-lg">Name</span>
           </label>
-          <input type="text" v-model="from_name" name="name" placeholder="John Doe" class="input input-bordered w-full mb-2" />
+          <input type="text" v-model="from_name" name="name" placeholder="John Doe" class="bg-white input input-bordered w-full mb-2" />
         </div>
         <div class="form-control w-full mb-2">
           <label class="label">
             <span class="label-text lg:text-lg">Email</span>
           </label>
-          <input type="text" placeholder="john@gmail.com" v-model="from_email" name="email" class="input input-bordered w-full mb-2" />
+          <input type="text" placeholder="john@gmail.com" v-model="from_email" name="email" class="bg-white input input-bordered w-full mb-2" />
         </div>
         <div class="form-control w-full mb-2">
           <label class="label">
             <span class="label-text lg:text-lg">Subject</span>
           </label>
-          <input type="text" v-model="subject" name="subject" class="input input-bordered w-full mb-2" />
+          <input type="text" v-model="subject" name="subject" class="bg-white input input-bordered w-full mb-2" />
         </div>
         <div class="form-control">
           <label class="label">
             <span class="label-text lg:text-lg">Message</span>
           </label>
-          <textarea v-model="message" name="message" class="textarea textarea-bordered h-24" ></textarea>
+          <textarea v-model="message" name="message" class="bg-white textarea textarea-bordered h-24" ></textarea>
         </div>
         <button class="btn btn-primary mt-4 rounded-none w-full mb-2 lg:text-lg" @click="sendEmail">Send</button>
       </div>
