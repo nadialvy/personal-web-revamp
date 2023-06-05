897ce7ce10d417c3c3283977361cdad946aca7fb
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2023-06-05 12:05:03 +0700

    add download cv

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -4,8 +4,10 @@
     <p class="text-3xl text-purple-500 font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl xl:pb-12">Let's Work Together</p>
     <div class="lg:flex lg:gap-x-24">
       <div class="lg:w-1/2 xl:text-xl">
+
         <p class="text-xl text-purple-500 mt-6 font-semibold 2xl:text-3xl">Contact Information</p>
         <p class="text-black mt-1 lg:text-lg xl:text-xl">Get in touch with me via the contact information below if you're interested in connecting, collaborating, or discussing potential opportunities:</p>
+
         <div class="flex gap-x-2 flex-wrap justify-start items-center mt-4 2xl:mt-16">
           <img src="/contact/email.png" alt="Email" class="w-7">
           <p class="hover:font-semibold text-purple-500 hover:text-pink-700 transition-all hover:cursor-pointer">nadialovely1803@gmail.com</p>
@@ -22,6 +24,8 @@
             <p class="hover:font-semibold text-purple-500 hover:text-pink-700 transition-all">nadialvy</p>
           </div>
         </a>
+      
+        <button class="mt-4 2xl:mt-16 btn bg-pink-500 hover:bg-pink-600 border-none rounded-none w-full mb-2 lg:text-lg normal-case" @click="downloadResume">Download my resume</button>
         <div class="divider lg:hidden"></div>
       </div>
 
@@ -50,7 +54,7 @@
           </label>
           <textarea v-model="message" name="message" class="bg-white textarea textarea-bordered h-24" ></textarea>
         </div>
-        <button class="btn btn-primary mt-4 rounded-none w-full mb-2 lg:text-lg" @click="sendEmail">Send</button>
+        <button class="btn btn-primary mt-4 rounded-none w-full mb-2 lg:text-lg normal-case" @click="sendEmail">Send</button>
       </div>
     </div>
     <img src="/contact/star.svg" alt="star" class="absolute top-0 -right-20 hidden lg:block w-64 -z-0 opacity-30 2xl:w-96 2xl:-right-32">
@@ -97,6 +101,24 @@ export default{
       this.name = ''
       this.email = ''
       this.message = ''
+    },
+    downloadResume(){
+      const fileURL = 'contact/resume.pdf';
+
+      // Create a temporary link element
+      const link = document.createElement('a');
+      link.href = fileURL;
+      link.target = '_blank';
+      link.download = 'Nadia Lovely Resume.pdf';
+
+      // Append the link element to the document body
+      document.body.appendChild(link);
+
+      // Trigger the click event on the link element
+      link.click();
+
+      // Clean up by removing the link element
+      document.body.removeChild(link);
     },
     createAlert(message, type, timeout) {
       createToast(message, {
