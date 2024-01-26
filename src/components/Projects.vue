16cbb78edc909e63842f5feb839b81f9ef9b9b69
nadia_lovely_29rpl <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2024-01-26 21:58:02 +0700

    feat: update the projects section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,98 +2,352 @@
   <p class="relative bottom-[68px] lg:bottom-[98px] bg-white" id="projects"></p>
   <div class="px-8 py-6 lg:px-20 xl:px-28 2xl:px-72 lg:py-10 relative bg-white">
     <!-- <p class="text-center font-bold text-red-500 lg:text-lg mb-4">!!! This page is under construction !!!</p> -->
-    <p class="text-3xl mb-6 text-purple-500 font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl">Projects</p>
-    <img src="/project/stars.svg" alt="Stars" class="absolute z-30 w-28 top-10 -left-2 lg:w-36 lg:top-16 lg:-left-4 2xl:left-56 2xl:top-24">
-    <img src="/project/star.svg" alt="Star" class="absolute z-30 w-16 bottom-10 right-4 lg:w-24 2xl:bottom-12 2xl:right-56">
+    <p
+      class="text-3xl mb-6 text-purple-500 font-bold text-center lg:text-4xl lg:pb-8 xl:text-5xl 2xl:text-6xl"
+    >
+      Projects
+    </p>
+    <img
+      src="/project/stars.svg"
+      alt="Stars"
+      class="absolute z-30 w-28 top-10 -left-2 lg:w-36 lg:top-16 lg:-left-4 2xl:left-56 2xl:top-24"
+    />
+    <img
+      src="/project/star.svg"
+      alt="Star"
+      class="absolute z-30 w-16 bottom-10 right-4 lg:w-24 2xl:bottom-12 2xl:right-56"
+    />
     <div class="carousel gap-x-4 lg:gap-x-8 rounded-box mb-10">
       <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/gredu.png" alt="Gredu" class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-          <div class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">GREDU</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Contributed to building a mobile app with Flutter during my internship, creating features such as homeroom and resolving UI errors and bugs during the development phase despite having no prior Flutter experience.</p>
-            <a href="https://play.google.com/store/search?q=gredu&c=apps&hl=id" target="_blank">
-              <p class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline">See the app</p>
+          <img
+            src="/project/inilhoits.png"
+            alt="inilhoits"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl"
+          >
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              INILHOITS! 2024
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Performing UI slicing and API integration on the Inilhoits
+              website. Inilhoits is one of the largest international-scale
+              events hosted by ITS to introduce the university, especially to
+              high school students, to the general public.
+            </p>
+            <a href="https://inilho.its.ac.id/" target="_blank">
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                Visit the app
+              </p>
             </a>
           </div>
         </div>
       </div>
       <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/jualin.png" alt="Bakulan" class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-          <div class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">Bakulan</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Created Bakulan app's frontend UI as part of a business plan competition to better showcase our idea to judges.</p>
+          <img
+            src="/project/flexitrip.png"
+            alt="flexitrip"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl"
+          >
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Flexitrip
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Build a UI-only app for Flexitrip using Flutter, a platform
+              dedicated to elevating your holiday experience.<span
+                class="text-pink-700 font-medium"
+              >
+                #selfemployed</span
+              >
+            </p>
           </div>
         </div>
       </div>
       <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/besnik.png" alt="Besnik" class="w-52 lg:w-64 xl:w-72 2xl:w-80">
+          <img
+            src="/project/inlight.png"
+            alt="inlight"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl"
+          >
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Inlight
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Develop a company profile for Inlight. Inlight is a furniture
+              company specializing in the production and export of a diverse
+              range of products internationally.<span
+                class="text-pink-700 font-medium"
+                >#selfemployed</span
+              >
+            </p>
+            <a href="https://inlight.vercel.app/" target="_blank">
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                Visit the app
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="ml-4 text-center flex flex-col">
+          <img
+            src="/project/cvo.png"
+            alt="PT-CVO"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl"
+          >
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              PT CVO Corporation
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Develop a company profile for PT CVO Corporation. Please note that
+              this project is ongoing, and any identified bugs can be
+              disregarded at this stage.
+              <span class="text-pink-700 font-medium">#selfemployed</span>
+            </p>
+            <a
+              href="https://pt-cvo-company-profile.vercel.app/"
+              target="_blank"
+            >
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                Visit the app
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="ml-4 text-center flex flex-col">
+          <img
+            src="/project/gredu.png"
+            alt="Gredu"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80 md:text-lg lg:text-lg xl:text-2xl"
+          >
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              GREDU
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Contributed to building a mobile app with Flutter during my
+              internship, creating features such as homeroom and resolving UI
+              errors and bugs during the development phase despite having no
+              prior Flutter experience.
+            </p>
+            <a
+              href="https://play.google.com/store/search?q=gredu&c=apps&hl=id"
+              target="_blank"
+            >
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                Visit the app
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="ml-4 text-center flex flex-col">
+          <img
+            src="/project/jualin.png"
+            alt="Bakulan"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
           <div class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">Profile Company</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Made a UI design exercise using Tailwind CSS within a month of starting to learn the framework.</p>
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Bakulan
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Created Bakulan app's frontend UI as part of a business plan
+              competition to better showcase our idea to judges.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="ml-4 text-center flex flex-col">
+          <img
+            src="/project/besnik.png"
+            alt="Besnik"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
+          <div class="w-52 lg:w-64 xl:w-72 2xl:w-80">
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Profile Company
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Made a UI design exercise using Tailwind CSS within a month of
+              starting to learn the framework.
+            </p>
             <a href="https://besnikbangking.netlify.app/" target="_blank">
-              <p class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline">See the web</p>
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                See the web
+              </p>
             </a>
           </div>
         </div>
       </div>
       <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/artcademy.png" alt="Artcademy" class="w-52 lg:w-64 xl:w-72 2xl:w-80">
+          <img
+            src="/project/artcademy.png"
+            alt="Artcademy"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
           <div class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">Artcademy</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Conceived Artcademy, an art online course platform, and transformed it into a real website to better showcase our idea during a business plan competition.</p>
-            <a href="https://artcademy-web-git-master-nadialvy.vercel.app/" target="_blank">
-              <p class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline">See the web</p>
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Artcademy
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Conceived Artcademy, an art online course platform, and
+              transformed it into a real website to better showcase our idea
+              during a business plan competition.
+            </p>
+            <a
+              href="https://artcademy-web-git-master-nadialvy.vercel.app/"
+              target="_blank"
+            >
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                See the web
+              </p>
             </a>
           </div>
         </div>
       </div>
       <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/siordig.png" alt="Siordig" class="w-52 lg:w-64 xl:w-72 2xl:w-80">
+          <img
+            src="/project/siordig.png"
+            alt="Siordig"
+            class="w-52 lg:w-64 xl:w-72 2xl:w-80"
+          />
           <div class="w-52 lg:w-64 xl:w-72 2xl:w-80">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">Siordig</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Implemented Siordig's UI into a mobile app and utilized data fetching to deliver information to users.</p>
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Siordig
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Implemented Siordig's UI into a mobile app and utilized data
+              fetching to deliver information to users.
+            </p>
           </div>
         </div>
       </div>
-      <div class="carousel-item">
+      <!-- <div class="carousel-item">
         <div class="ml-4 text-center flex flex-col">
-          <img src="/project/POS.png" alt="Wikusama Cafe" class="w-[575px] lg:w-[700px] xl:w-[800px] 2xl:w-[880px]">
+          <img
+            src="/project/POS.png"
+            alt="Wikusama Cafe"
+            class="w-[575px] lg:w-[700px] xl:w-[800px] 2xl:w-[880px]"
+          />
           <div class="w-72 lg:w-[700px] xl:w-[800px] 2xl:w-[880px]">
-            <p class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8">Wikusama Cafe POS</p>
-            <p class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2">Developed a restaurant point of sales system for a school project using VueJS and Tailwind for the frontend, and Laravel and MySQL for the backend. Although not optimized for mobile view due to time constraints, the project showcased proficiency in these technologies.</p>
-            <a href="https://github.com/nadialvy/wikusama-cafe-fe" target="_blank">
-              <p class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline">See the repository</p>
+            <p
+              class="lg:text-lg xl:text-xl 2xl:text-2xl text-lg text-purple-500 font-bold mt-2 xl:mt-4 2xl:mt-8"
+            >
+              Wikusama Cafe POS
+            </p>
+            <p
+              class="text-sm lg:text-lg xl:text-xl text-justify text-purple-500 mt-2"
+            >
+              Developed a restaurant point of sales system for a school project
+              using VueJS and Tailwind for the frontend, and Laravel and MySQL
+              for the backend. Although not optimized for mobile view due to
+              time constraints, the project showcased proficiency in these
+              technologies.
+            </p>
+            <a
+              href="https://github.com/nadialvy/wikusama-cafe-fe"
+              target="_blank"
+            >
+              <p
+                class="text-sm lg:text-lg xl:text-xl mt-4 text-pink-700 text-start underline"
+              >
+                See the repository
+              </p>
             </a>
           </div>
         </div>
-      </div>
+      </div> -->
     </div>
-
   </div>
 </template>
 
 <style>
-  .flex-container {
-    display: flex;
-    flex-wrap: nowrap;
-    overflow-x: auto;
-  }
-  .card {
-    display: inline-block;
-    margin: 0 10px;
-    width: 300px;
-    background-color: white;
-    border-radius: 5px;
-    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
-    padding: 20px;
-  }
-  /* Hide scrollbar */
-  .flex-container::-webkit-scrollbar {
-    display: none;
-  }
+.flex-container {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+}
+.card {
+  display: inline-block;
+  margin: 0 10px;
+  width: 300px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05);
+  padding: 20px;
+}
+/* Hide scrollbar */
+.flex-container::-webkit-scrollbar {
+  display: none;
+}
 </style>
